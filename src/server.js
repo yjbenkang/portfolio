45d0b3b7cde12c10106f31395c8fffd5511b7b29
강용진 <kyongjin1234@gmail.com>
2021-09-04 14:49:52 +0900

    PORT variable

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import { indexController } from "./controller";
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 const logger = morgan("dev");
 

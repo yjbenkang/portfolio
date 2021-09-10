fc936f7695c29b300180237cf87f351dd5acb8b9
강용진 <kyongjin1234@gmail.com>
2021-09-10 16:56:15 +0900

    web develop

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -4,18 +4,43 @@ const sectorProjects = document.querySelector(".sector__projects");
 const projects = document.querySelectorAll('.project-mixin')
 let scrollPosition = 0;
 
+
+const handleHoverProject= (event,i) => {
+    const project = projects[i];
+    const preDescription = project.querySelector(".preDescription");
+    const Image = project.style.backgroundImage;
+    project.style.backgroundImage="none";
+    project.style.backgroundColor="rgb(25, 188, 155)"; 
+    preDescription.innerText="hi";    
+
+    
+    project.addEventListener("mouseleave", (e) => {
+        project.style.backgroundImage=Image;
+        project.style.backgroundColor="none"; 
+        preDescription.innerText="";
+        
+
+        
+    })
+
+
+};
 const handleClickProject = (event, i) => {
-    projects[i].classList.remove('project-mixin__hover');
+    const project = projects[i];
+    const preDescription = project.querySelector(".preDescription");
+    preDescription.innerText="";
     const modalWrapList = document.querySelectorAll('.modal_wrap');
+    project.classList.remove('project-mixin__hover');
     const modalWrap = modalWrapList[i];
     modalWrap.style.display ='flex';
     modalWrap.style.cursor = 'initial';
+    project.removeEventListener("mouseenter", (event) =>handleHoverProject(event,i));
     const blackBg = document.querySelector('.black_bg')
     blackBg.style.display ='block';
     blackBg.style.cursor = 'pointer';
 
     blackBg.addEventListener("click", (e) => {
-        projects[i].classList.add('project-mixin__hover');
+        project.classList.add('project-mixin__hover');
         const evTarget = e.target
         if (!evTarget.classList.contains("modal_wrap")) {
             modalWrap.style.display = "none";
@@ -26,6 +51,7 @@ const handleClickProject = (event, i) => {
 
 for (let i = 0; i < projects.length; ++i) {
     let project = projects[i]; 
+    project.addEventListener("mouseenter", (event) =>handleHoverProject(event,i));
     project.addEventListener("click", (event) => handleClickProject(event, i));
 }
 

3e55ed20f911585c8bf948b3f116293346117a08
강용진 <kyongjin1234@gmail.com>
2021-09-10 18:08:07 +0900

    마우스 호버링시 돋보기 나오기

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -5,54 +5,54 @@ const projects = document.querySelectorAll('.project-mixin')
 let scrollPosition = 0;
 
 
-// const handleHoverProject= (event,i) => {
-//     const project = projects[i];
-//     const preDescription = project.querySelector(".preDescription");
-//     const Image = project.style.backgroundImage;
-//     project.style.backgroundImage="none";
-//     project.style.backgroundColor="rgb(25, 188, 155)"; 
-//     preDescription.innerText="hi";    
+const handleHoverProject= (event,i) => {
+    const project = projects[i];
+    const Image = project.style.backgroundImage;
+    const letsee = document.createElement('i');
+    project.style.backgroundImage="none";
+    project.style.backgroundColor="rgb(25, 188, 155)"; 
+    letsee.className="fas fa-search fa-5x";
+    letsee.style.position= "absolute";
+    letsee.style.right= "42%";
+    letsee.style.top="30%";
+    project.insertBefore(letsee,project.firstChild);
+    project.addEventListener("mouseleave", (e) => {
+        project.style.backgroundImage=Image;
+        project.style.backgroundColor="none"; 
+        project.removeChild(letsee);
+ 
+    })
+    project.addEventListener("click", (event) => {
+        const project = projects[i];
+        const modalWrapList = document.querySelectorAll('.modal_wrap');
+        project.classList.remove('project-mixin__hover');
+        const modalWrap = modalWrapList[i];
+        modalWrap.style.display ='flex';
+        modalWrap.style.cursor = 'initial';
+        const blackBg = document.querySelector('.black_bg')
+        blackBg.style.display ='block';
+        blackBg.style.cursor = 'pointer';
+    
+        blackBg.addEventListener("click", (e) => {
+            project.classList.add('project-mixin__hover');
+            const evTarget = e.target
+            if (!evTarget.classList.contains("modal_wrap")) {
+                modalWrap.style.display = "none";
+                blackBg.style.display = "none";
+    
+            }
+        })
+    });
 
     
-//     project.addEventListener("mouseleave", (e) => {
-//         project.style.backgroundImage=Image;
-//         project.style.backgroundColor="none"; 
-//         preDescription.innerText="";
-        
-
-        
-//     })
 
 
-// };
-const handleClickProject = (event, i) => {
-    const project = projects[i];
-    const preDescription = project.querySelector(".preDescription");
-    preDescription.innerText="";
-    const modalWrapList = document.querySelectorAll('.modal_wrap');
-    project.classList.remove('project-mixin__hover');
-    const modalWrap = modalWrapList[i];
-    modalWrap.style.display ='flex';
-    modalWrap.style.cursor = 'initial';
-    project.removeEventListener("mouseenter", (event) =>handleHoverProject(event,i));
-    const blackBg = document.querySelector('.black_bg')
-    blackBg.style.display ='block';
-    blackBg.style.cursor = 'pointer';
-
-    blackBg.addEventListener("click", (e) => {
-        project.classList.add('project-mixin__hover');
-        const evTarget = e.target
-        if (!evTarget.classList.contains("modal_wrap")) {
-            modalWrap.style.display = "none";
-            blackBg.style.display = "none";
-        }
-    })
 };
 
 for (let i = 0; i < projects.length; ++i) {
     let project = projects[i]; 
     project.addEventListener("mouseenter", (event) =>handleHoverProject(event,i));
-    project.addEventListener("click", (event) => handleClickProject(event, i));
+
 }
 
 
